33f1517b673db3b24e80d5439e930be9faf8ad89
firstrow <firstrow@gmail.com>
2011-11-01 00:30:19 +0200

    Added base filer icon to SGridView

--- protected/modules/admin/widgets/SGridView.php
+++ protected/modules/admin/widgets/SGridView.php
@@ -7,4 +7,29 @@ class SGridView extends CGridView {
 	public $cssFile = false;
 	public $template = '{items}{summary}{pager}';
 
+	/**
+	 * Renders the data items for the grid view.
+	 */
+	public function renderItems()
+	{
+		if($this->dataProvider->getItemCount()>0 || $this->showTableOnEmpty)
+		{
+			echo '
+				<div class="gridViewOptions">
+					&nbsp;
+				</div>
+			';
+			echo "<table class=\"{$this->itemsCssClass}\">\n";
+			$this->renderTableHeader();
+			ob_start();
+			$this->renderTableBody();
+			$body=ob_get_clean();
+			$this->renderTableFooter();
+			echo $body; // TFOOT must appear before TBODY according to the standard.
+			echo "</table>";
+		}
+		else
+			$this->renderEmptyText();
+	}
+
 }
