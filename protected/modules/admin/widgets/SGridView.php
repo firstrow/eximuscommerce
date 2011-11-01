04024bd9fbfc520393200b3dddbfeee1b11d957d
firstrow <firstrow@gmail.com>
2011-11-01 21:50:23 +0200

    Added test code to grid

--- protected/modules/admin/widgets/SGridView.php
+++ protected/modules/admin/widgets/SGridView.php
@@ -2,6 +2,31 @@
 
 Yii::import('zii.widgets.grid.CGridView');
 
+// $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+//     'id'=>'mydialog',
+//     // additional javascript options for the dialog plugin
+//     'options'=>array(
+//         'title'=>'Dialog box 1',
+//         'modal'=>true,
+//         'resizable'=>false,
+//         'draggable'=>false,
+//         'autoOpen'=>false,
+//         'buttons'=>array(
+//             'Сохранить'=>'js:function(){alert("ok")}',
+//             'Отмена'=>'js:function(){alert("cancel")}',
+//         ),
+//     ),
+// ));
+
+// echo 'dialog content here';
+
+// $this->endWidget('zii.widgets.jui.CJuiDialog');
+
+// // the link that may open the dialog
+// echo CHtml::link('open dialog', '#', array(
+//    'onclick'=>'$("#mydialog").dialog("open"); return false;',
+// ));
+
 class SGridView extends CGridView {
 	
 	public $template = '{items}{summary}{pager}';
