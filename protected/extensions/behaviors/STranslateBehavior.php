6c617fe579cb7a2838c3a338885e155dbc9ce945
firstrow <firstrow@gmail.com>
2012-03-02 20:00:42 +0200

    Converted indents

--- protected/extensions/behaviors/STranslateBehavior.php
+++ protected/extensions/behaviors/STranslateBehavior.php
@@ -40,138 +40,138 @@ class STranslateBehavior extends CActiveRecordBehavior {
 	 */
 	public $translateAttributes = array();
 
-    /**
-     * @var integer Language id used to load model translation data.
-     * If null active language id we'll be used.
-     */
-    private $_translation_lang;
+	/**
+	 * @var integer Language id used to load model translation data.
+	 * If null active language id we'll be used.
+	 */
+	private $_translation_lang;
 
-    public function attach($owner)
-    {
-        return parent::attach($owner);
-    }
+	public function attach($owner)
+	{
+		return parent::attach($owner);
+	}
 
-    /**
-     * Merge object query with translate query.
-     */
-    public function beforeFind()
-    {
-        $this->owner->getDbCriteria()->mergeWith(array(
-            'with'=>array('translate'=>array(
-                'condition'=>'translate.language_id=:language_id',
-                'params'=>array(
-                    ':language_id'=>$this->getTranslateLanguageId()
-                )
-            )),
-        ));
-        return true;
-    }
+	/**
+	 * Merge object query with translate query.
+	 */
+	public function beforeFind()
+	{
+		$this->owner->getDbCriteria()->mergeWith(array(
+			'with'=>array('translate'=>array(
+				'condition'=>'translate.language_id=:language_id',
+				'params'=>array(
+					':language_id'=>$this->getTranslateLanguageId()
+				)
+			)),
+		));
+		return true;
+	}
 
-    /**
-     * Get language id to load translated data.
-     * @return integer Language id
-     */
-    public function getTranslateLanguageId()
-    {
-        if ($this->_translation_lang)
-            return $this->_translation_lang;
-        return Yii::app()->languageManager->active->id;
-    }
+	/**
+	 * Get language id to load translated data.
+	 * @return integer Language id
+	 */
+	public function getTranslateLanguageId()
+	{
+		if ($this->_translation_lang)
+			return $this->_translation_lang;
+		return Yii::app()->languageManager->active->id;
+	}
 
 	/**
 	 * Apply object translation
 	 */
-    public function afterFind()
-    {
-        if($this->owner->translate)
-        {
-            foreach($this->translateAttributes as $attr)
-                $this->owner->$attr = $this->owner->translate[$attr];
-        }
-        return true;
-    }
+	public function afterFind()
+	{
+		if($this->owner->translate)
+		{
+			foreach($this->translateAttributes as $attr)
+				$this->owner->$attr = $this->owner->translate[$attr];
+		}
+		return true;
+	}
 
-    /**
-     * Update model translations
-     */
-    public function afterSave()
-    {
-        $translate = $this->owner->translate;
-        if ($this->owner->isNewRecord OR !$translate)
-            $this->insertTranslations();
-        else
-            $this->updateTranslation($translate);
-        return true;
-    }
+	/**
+	 * Update model translations
+	 */
+	public function afterSave()
+	{
+		$translate = $this->owner->translate;
+		if ($this->owner->isNewRecord OR !$translate)
+			$this->insertTranslations();
+		else
+			$this->updateTranslation($translate);
+		return true;
+	}
 
-    /**
-     * Create new object translation for each language.
-     * Used on creating new object.
-     */
-    public function insertTranslations()
-    {
-        foreach (Yii::app()->languageManager->languages as $lang)
-            $this->createTranslation($lang->id);
-    }
+	/**
+	 * Create new object translation for each language.
+	 * Used on creating new object.
+	 */
+	public function insertTranslations()
+	{
+		foreach (Yii::app()->languageManager->languages as $lang)
+			$this->createTranslation($lang->id);
+	}
 
-    /**
-     * Create object translation
-     * @param $languageId Language id
-     * @return boolean Translation save result
-     */
-    public function createTranslation($languageId)
-    {
-        $className = $this->owner->translateModelName;
-        $translate = new $className;
-        $translate->object_id = $this->owner->getPrimaryKey();
-        $translate->language_id = $languageId;
+	/**
+	 * Create object translation
+	 * @param $languageId Language id
+	 * @return boolean Translation save result
+	 */
+	public function createTranslation($languageId)
+	{
+		$className = $this->owner->translateModelName;
+		$translate = new $className;
+		$translate->object_id = $this->owner->getPrimaryKey();
+		$translate->language_id = $languageId;
 
-        // Populate translated attributes
-        foreach($this->translateAttributes as $attr)
-            $translate->$attr = $this->owner->$attr;
+		// Populate translated attributes
+		foreach($this->translateAttributes as $attr)
+			$translate->$attr = $this->owner->$attr;
 
-        return $translate->save(false);
-    }
+		return $translate->save(false);
+	}
 
-    /**
-     * Update "current" translation object
-     * @param CActiveRecord $translate
-     */
-    public function updateTranslation($translate)
-    {
-        foreach ($this->translateAttributes as $attr)
-            $translate->$attr = $this->owner->$attr;
-        $translate->save(false);
-    }
+	/**
+	 * Update "current" translation object
+	 * @param CActiveRecord $translate
+	 */
+	public function updateTranslation($translate)
+	{
+		foreach ($this->translateAttributes as $attr)
+			$translate->$attr = $this->owner->$attr;
+		$translate->save(false);
+	}
 
-    /**
-     * Delete model related translations
-     */
-    public function afterDelete()
-    {
-        $className = $this->owner->translateModelName;
-        $className::model()
-            ->deleteAll('object_id=:id',array(
-                ':id'=>$this->owner->getPrimaryKey()
-            ));
-        return true;
-    }
+	/**
+	 * Delete model related translations
+	 */
+	public function afterDelete()
+	{
+		$className = $this->owner->translateModelName;
+		$className::model()
+			->deleteAll('object_id=:id',array(
+				':id'=>$this->owner->getPrimaryKey()
+			));
+		return true;
+	}
 
-    /**
-     * Scope to load translation by language id
-     * @param mixed $language or array containing `lang_id` key
-     * @return CActiveRecord
-     */
-    public function language($language=null)
-    {
-        if(is_array($language) && isset($language['lang_id']))
-            $language = $language['lang_id'];
+	/**
+	 * Scope to load translation by language id
+	 * @param mixed $language or array containing `lang_id` key
+	 * @return CActiveRecord
+	 */
+	public function language($language=null)
+	{
+		if(is_array($language) && isset($language['lang_id']))
+			$language = $language['lang_id'];
 
-        if(!Yii::app()->languageManager->getById($language))
-            $language = Yii::app()->languageManager->default->id;
+		if(!Yii::app()->languageManager->getById($language))
+			$language = Yii::app()->languageManager->default->id;
 
-        $this->_translation_lang = $language;
-        return $this->owner;
-    }
+		$this->_translation_lang = $language;
+		return $this->owner;
+	}
 
 }
