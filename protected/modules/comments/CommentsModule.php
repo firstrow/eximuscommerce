7abd85e188116d237b00b69c2ccdb7d738c9d43d
firstrow <firstrow@gmail.com>
2012-03-02 10:11:19 +0200

    Refactored comments modules

--- protected/modules/comments/CommentsModule.php
+++ protected/modules/comments/CommentsModule.php
@@ -17,4 +17,31 @@ class CommentsModule extends BaseModule
 		));
 	}
 
+	public function processRequest($model)
+	{
+		$comment = new Comment;
+		if(Yii::app()->request->isPostRequest)
+		{
+			$comment->attributes = Yii::app()->request->getPost('Comment');
+
+			if(!Yii::app()->user->isGuest)
+			{
+				$comment->name = Yii::app()->user->name;
+				$comment->email = Yii::app()->user->email;
+			}
+
+			if($comment->validate())
+			{
+				$comment->class_name = get_class($model);
+				$comment->object_pk = $model->id;
+				$comment->user_id = Yii::app()->user->isGuest ? 0 : Yii::app()->user->id;
+				$comment->save();
+
+				// Refresh page
+				Yii::app()->request->redirect(Yii::app()->getRequest()->getUrl(), true);
+			}
+		}
+		return $comment;
+	}
+
 }
--- protected/modules/comments/views/comment/create.php
+++ protected/modules/comments/views/comment/create.php
@@ -6,32 +6,10 @@
 
 // Load module
 $module = Yii::app()->getModule('comments');
+// Validate and save comment on post request
+$comment = $module->processRequest($model);
 // Load model comments
 $comments = Comment::getObjectComments($model);
-
-$comment = new Comment();
-
-if(Yii::app()->request->isPostRequest)
-{
-	$comment->attributes = Yii::app()->request->getPost('Comment');
-
-	if(!Yii::app()->user->isGuest)
-	{
-		$comment->name = Yii::app()->user->name;
-		$comment->email = Yii::app()->user->email;
-	}
-
-	if($comment->validate())
-	{
-		$comment->class_name = get_class($model);
-		$comment->object_pk = $model->id;
-		$comment->user_id = Yii::app()->user->isGuest ? 0 : Yii::app()->user->id;
-		$comment->save();
-
-		// Refresh page
-		$this->refresh();
-	}
-}
 
 // Display comments
 if(!empty($comments))
