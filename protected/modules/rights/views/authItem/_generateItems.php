488ed98299f52a2e665a9ce7fecd1b1f729ba7fb
firstrow <firstrow@gmail.com>
2012-04-19 20:16:36 +0300

    Fixed rights module look&feel

--- protected/modules/rights/views/authItem/_generateItems.php
+++ protected/modules/rights/views/authItem/_generateItems.php
@@ -1,9 +1,6 @@
 <?php if( $items['controllers']!==array() ): ?>
-
 	<?php foreach( $items['controllers'] as $key=>$item ): ?>
-
 		<?php if( isset($item['actions'])===true && $item['actions']!==array() ): ?>
-
 			<?php $controllerKey = isset($moduleName)===true ? ucfirst($moduleName).'.'.$item['name'] : $item['name']; ?>
 			<?php $controllerExists = isset($existingItems[ $controllerKey.'.*' ]); ?>
 
@@ -14,7 +11,6 @@
 			</tr>
 
 			<?php $i=0; foreach( $item['actions'] as $action ): ?>
-
 				<?php $actionKey = $controllerKey.'.'.ucfirst($action['name']); ?>
 				<?php $actionExists = isset($existingItems[ $actionKey ]); ?>
 
@@ -23,31 +19,20 @@
 					<td class="name-column"><?php echo $action['name']; ?></td>
 					<td class="path-column"><?php echo substr($item['path'], $basePathLength+1).(isset($action['line'])===true?':'.$action['line']:''); ?></td>
 				</tr>
-
 			<?php endforeach; ?>
-
 		<?php endif; ?>
-
 	<?php endforeach; ?>
-
 <?php else: ?>
-
 	<tr><th  class="no-items-row" colspan="3"><?php echo Rights::t('core', 'No actions found.'); ?></th></tr>
-
 <?php endif; ?>
 
 <?php if( $items['modules']!==array() ): ?>
-
 	<?php if( $displayModuleHeadingRow===true ): ?>
-
 		<tr><th class="module-heading-row" colspan="3"><?php echo Rights::t('core', 'Modules'); ?></th></tr>
-
 	<?php endif; ?>
 
 	<?php foreach( $items['modules'] as $moduleName=>$moduleItems ): ?>
-
 		<tr><th class="module-row" colspan="3"><?php echo ucfirst($moduleName).'Module'; ?></th></tr>
-
 		<?php $this->renderPartial('_generateItems', array(
 			'model'=>$model,
 			'form'=>$form,
@@ -57,7 +42,5 @@
 			'displayModuleHeadingRow'=>false,
 			'basePathLength'=>$basePathLength,
 		)); ?>
-
 	<?php endforeach; ?>
-
 <?php endif; ?>
--- protected/modules/rights/views/authItem/generate.php
+++ protected/modules/rights/views/authItem/generate.php
@@ -1,28 +1,21 @@
 <?php $this->breadcrumbs = array(
-	'Rights'=>Rights::getBaseUrl(),
+	'Home'=>$this->createUrl('/admin'),
+	'Права доступа'=>Rights::getBaseUrl(),
 	Rights::t('core', 'Generate items'),
 ); ?>
 
-<div id="generator">
-
-	<h2><?php echo Rights::t('core', 'Generate items'); ?></h2>
-
+<div id="generator" class="padding-all">
+	<h3><?php echo Rights::t('core', 'Generate items'); ?></h3>
 	<p><?php echo Rights::t('core', 'Please select which items you wish to generate.'); ?></p>
 
 	<div class="form">
-
 		<?php $form=$this->beginWidget('CActiveForm'); ?>
-
 			<div class="row">
-
 				<table class="items generate-item-table" border="0" cellpadding="0" cellspacing="0">
-
 					<tbody>
-
 						<tr class="application-heading-row">
 							<th colspan="3"><?php echo Rights::t('core', 'Application'); ?></th>
 						</tr>
-
 						<?php $this->renderPartial('_generateItems', array(
 							'model'=>$model,
 							'form'=>$form,
@@ -31,15 +24,11 @@
 							'displayModuleHeadingRow'=>true,
 							'basePathLength'=>strlen(Yii::app()->basePath),
 						)); ?>
-
 					</tbody>
-
 				</table>
-
 			</div>
 
 			<div class="row">
-
    				<?php echo CHtml::link(Rights::t('core', 'Select all'), '#', array(
    					'onclick'=>"jQuery('.generate-item-table').find(':checkbox').attr('checked', 'checked'); return false;",
    					'class'=>'selectAllLink')); ?>
@@ -47,17 +36,12 @@
 				<?php echo CHtml::link(Rights::t('core', 'Select none'), '#', array(
 					'onclick'=>"jQuery('.generate-item-table').find(':checkbox').removeAttr('checked'); return false;",
 					'class'=>'selectNoneLink')); ?>
-
 			</div>
 
    			<div class="row">
-
 				<?php echo CHtml::submitButton(Rights::t('core', 'Generate')); ?>
-
 			</div>
 
 		<?php $this->endWidget(); ?>
-
 	</div>
-
 </div>
