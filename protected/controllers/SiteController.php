8c83945d8866d1aac92618cd8cd1b954fb0f0c29
firstrow <firstrow@gmail.com>
2012-04-19 21:53:53 +0300

    Added database dumper

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,54 +2,44 @@
 
 class SiteController extends Controller {
 
-    public function filters()
-    {
-        return array(
-            'rights',
-        );
-    }
+	public function filters()
+	{
+		return array(
+			'rights',
+		);
+	}
 
-    public function allowedActions()
-    {
-        return 'login, index, error, contact';
-    }
+	public function allowedActions()
+	{
+		return 'login, index, error, contact';
+	}
 
 
-    public function actionLogin()
-    {
+	public function actionLogin()
+	{
 
-    }
+	}
 
-    public function actionContact()
-    {
-	    Yii::import('store.models.*');
-		$product = StoreProduct::model()->findByPk(56);
-	    var_dump($product->name);
-	    $category = $product->mainCategory;
-	    var_dump($category->name);
-	    var_dump($product->name);
-    }
+	public function actionIndex()
+	{
+		echo $this->createUrl('site/index', array('name'=>'sdsd'));
+	}
 
-    public function actionIndex()
-    {
-            echo $this->createUrl('site/index', array('name'=>'sdsd'));
-    }
+	/**
+	 * This is the action to handle external exceptions.
+	 */
+	public function actionError()
+	{
+		$error=Yii::app()->errorHandler->error;
+		if($error)
+		{
+			if(Yii::app()->request->isAjaxRequest)
+				echo $error['message'];
+			else
+				var_dump($error);
+		}else{
+			echo 'Some error hapenned!';
+		}
 
-    /**
-     * This is the action to handle external exceptions.
-     */
-    public function actionError()
-    {
-        $error=Yii::app()->errorHandler->error;
-        if($error)
-        {
-            if(Yii::app()->request->isAjaxRequest)
-                echo $error['message'];
-            else
-                var_dump($error);
-        }else{
-            echo 'Some error hapenned!';
-        }
-
-    }
+	}
 }
