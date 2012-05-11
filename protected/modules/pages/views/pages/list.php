e524ac393db9dd08d0fed6222e680618482c91b9
firstrow <firstrow@gmail.com>
2012-05-11 21:45:42 +0300

    Added static pages styles

--- protected/modules/pages/views/pages/list.php
+++ protected/modules/pages/views/pages/list.php
@@ -1,22 +1,27 @@
 <?php
-	// Set meta tags
-	$this->pageTitle = ($model->meta_title) ? $model->meta_title : $model->name;
-	$this->pageKeywords = $model->meta_keywords;
-	$this->pageDescription = $model->meta_description;
 
-	$this->breadcrumbs = array(
-		$model->name
-    );
+/**
+ * View category pages
+ * @var PageCategory $model
+ */
+
+// Set meta tags
+$this->pageTitle = ($model->meta_title) ? $model->meta_title : $model->name;
+$this->pageKeywords = $model->meta_keywords;
+$this->pageDescription = $model->meta_description;
 ?>
 
-<?php if (sizeof($pages) > 0): ?>
-	<?php foreach ($pages as $page): ?>
-		<?php echo $page->title ?><br/>
-	<?php endforeach ?>
-<?php else: ?>
-	<?php echo Yii::t('PagesModule.core', 'В категории нет страниц.') ?>
-<?php endif ?>
+<h1 class="has_background"><?php echo $model->name ?></h1>
+<p>
+	<?php if (sizeof($pages) > 0): ?>
+		<?php foreach ($pages as $page): ?>
+			<?php echo CHtml::link($page->title, array('/pages/pages/view', 'url'=>$page->url)); ?><br/>
+		<?php endforeach ?>
+	<?php else: ?>
+		<?php echo Yii::t('PagesModule.core', 'В категории нет страниц.') ?>
+	<?php endif ?>
 
-<?php $this->widget('CLinkPager', array(
-    'pages' => $pagination,
-)) ?>
+	<?php $this->widget('CLinkPager', array(
+		'pages' => $pagination,
+	)) ?>
+</p>
--- protected/modules/pages/views/pages/view.php
+++ protected/modules/pages/views/pages/view.php
@@ -1,15 +1,17 @@
 <?php
-	// Set meta tags
-	$this->pageTitle = ($model->meta_title) ? $model->meta_title : $model->title;
-	$this->pageKeywords = $model->meta_keywords;
-	$this->pageDescription = $model->meta_description;
 
-	$this->breadcrumbs = array(
-		$model->title
-    );
+/**
+ * View page
+ * @var Page $model
+ */
+
+// Set meta tags
+$this->pageTitle       = ($model->meta_title) ? $model->meta_title : $model->title;
+$this->pageKeywords    = $model->meta_keywords;
+$this->pageDescription = $model->meta_description;
 ?>
 
-<h3><?php echo $model->title; ?></h3>
+<h1 class="has_background"><?php echo $model->title; ?></h1>
 <p>
 	<?php echo $model->short_description; ?>
 </p>
