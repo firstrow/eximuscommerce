a8855c75138e8668bf7427fdaf8f020942219e3f
firstrow <firstrow@gmail.com>
2012-05-19 21:22:17 +0300

    Removed ToTop init

--- protected/modules/admin/assets/scripts/init.scripts.js
+++ protected/modules/admin/assets/scripts/init.scripts.js
@@ -1,27 +1,23 @@
 
-    <!-- Script to move navigation bar on scroll -->
-    $(window).load(function (){
-        var topBar = $('#navigation');
-        var start  = topBar.offset().top;
-        var fixed;
-        $(window).scroll(function () {
-            if(!fixed && (topBar.offset().top - $(window).scrollTop() < 0)){
-                $("#hd").css('margin-bottom','38px');
-                topBar.css('top', 0);
-                topBar.css('position', 'fixed');
-                topBar.css('width', $('#doc3').css('width'));
-                topBar.css('opacity', '0.92');
-                fixed = true;
-            }else if(fixed && $(window).scrollTop() <= start){
-                topBar.css('position', '');
-                topBar.css('width', '');
-                topBar.css('opacity', '1');
-                $("#hd").css('margin-bottom','');
-                fixed = false;
-            }
-        });
+<!-- Script to move navigation bar on scroll -->
+$(window).load(function (){
+    var topBar = $('#navigation');
+    var start  = topBar.offset().top;
+    var fixed;
+    $(window).scroll(function () {
+        if(!fixed && (topBar.offset().top - $(window).scrollTop() < 0)){
+            $("#hd").css('margin-bottom','38px');
+            topBar.css('top', 0);
+            topBar.css('position', 'fixed');
+            topBar.css('width', $('#doc3').css('width'));
+            topBar.css('opacity', '0.92');
+            fixed = true;
+        }else if(fixed && $(window).scrollTop() <= start){
+            topBar.css('position', '');
+            topBar.css('width', '');
+            topBar.css('opacity', '1');
+            $("#hd").css('margin-bottom','');
+            fixed = false;
+        }
     });
-
-    jQuery(document).ready(function(){
-        $().UItoTop({ easingType: 'easeOutQuart' });
-    });
+});
