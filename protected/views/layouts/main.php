4ba89b366ba05843d3e50a90e6ab83bc89e13592
firstrow <firstrow@gmail.com>
2012-06-09 12:00:05 +0300

    Updated layouts

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -109,11 +109,14 @@
 		<?php
 			Yii::import('application.modules.store.models.StoreCategory');
 			$items = StoreCategory::model()->findByPk(1)->asCMenuArray();
-			$this->widget('application.extensions.mbmenu.MbMenu',array(
-					'cssFile'=>Yii::app()->theme->baseUrl.'/assets/css/menu.css',
-					'htmlOptions'=>array('class'=>'dropdown', 'id'=>'nav'),
-					'items'=>$items['items'])
-			);
+			if(isset($items['items']))
+			{
+				$this->widget('application.extensions.mbmenu.MbMenu',array(
+						'cssFile'=>Yii::app()->theme->baseUrl.'/assets/css/menu.css',
+						'htmlOptions'=>array('class'=>'dropdown', 'id'=>'nav'),
+						'items'=>$items['items'])
+				);
+			}
 		?>
 	</div>
 </div>
