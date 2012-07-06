5f5c4580169b41911f7b20ba5dd781c73bd0487f
firstrow <firstrow@gmail.com>
2012-07-07 00:09:14 +0300

    Added system settings test

---
+++ protected/modules/core/tests/functional/SystemSettingsWebTest.php
@@ -0,0 +1,43 @@
+<?php
+
+class SystemSettingsWebTest extends WebTestCase
+{
+	public function testSystemSettings()
+	{
+		$siteName=microtime();
+		$pp='1,2,'.rand(3,10);
+		$ppa=rand(3,10);
+
+		$this->adminLogin();
+		$this->open('/admin/core/systemSettings');
+		$this->type('id=SystemSettingsForm_siteName', $siteName);
+		$this->type('id=SystemSettingsForm_productsPerPage', $pp);
+		$this->type('id=SystemSettingsForm_productsPerPageAdmin', $ppa);
+		$this->clickAndWait('css=#save_topLink > span.ui-button-text');
+
+		Yii::import('application.modules.core.models.SystemSettingsForm');
+		$model=new SystemSettingsForm;
+		$this->assertEquals($model->siteName, $siteName);
+		$this->assertEquals($model->productsPerPage, $pp);
+		$this->assertEquals($model->productsPerPageAdmin, $ppa);
+		$this->assertEquals(Yii::app()->settings->get('core', 'siteName'), $siteName);
+	}
+
+	public function testTitle()
+	{
+		Yii::app()->settings->set('core', array(
+			'siteName'=>microtime()
+		));
+		$this->open('/');
+		$this->assertEquals(Yii::app()->settings->get('core','siteName'), $this->getTitle());
+
+		// Find any active product
+		$product = StoreProduct::model()->active()->find();
+		$this->assertTrue($product instanceof StoreProduct);
+
+		// Open product page and post comment
+		$this->open(Yii::app()->createUrl('/store/frontProduct/view', array('url'=>$product->url)));
+
+		$this->assertEquals($product->name.' / '.Yii::app()->settings->get('core','siteName'), $this->getTitle());
+	}
+}
