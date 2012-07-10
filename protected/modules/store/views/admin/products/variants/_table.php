7aee7d7383a031de37b07268786a0be77931a6c9
firstrow <firstrow@gmail.com>
2012-07-10 22:16:52 +0300

    Fixed input width on variants tab

--- protected/modules/store/views/admin/products/variants/_table.php
+++ protected/modules/store/views/admin/products/variants/_table.php
@@ -7,80 +7,80 @@
 ?>
 
 <table class="variantsTable" id="variantAttribute<?php echo $attribute->id ?>">
-    <thead>
-    <tr>
-        <td colspan="6">
-            <h4>
-                <?php
-                echo CHtml::encode($attribute->title);
-                echo CHtml::link(' +', '#', array(
-                    'rel'=>'#variantAttribute'.$attribute->id,
-                    'onclick'=>'js: return cloneVariantRow($(this));'
-                ));
-                ?>
-            </h4>
-        </td>
-    </tr>
-    <tr>
-        <td>Значение</td>
-        <td>Цена</td>
-        <td>Тип цены</td>
-        <td>Артикул</td>
-        <td></td>
-    </tr>
-    </thead>
-    <tbody>
-        <?php
-            if(!isset($options)):
-        ?>
-        <tr>
-            <td>
-                <?php
-                    echo CHtml::dropDownList('variants['.$attribute->id.'][option_id][]', null, CHtml::listData($attribute->options, 'id', 'value'));
-                ?>
-            </td>
-            <td>
-                <input type="text" name="variants[<?php echo $attribute->id ?>][price][]">
-            </td>
-            <td>
-                <?php echo CHtml::dropDownList('variants['.$attribute->id.'][price_type][]', null, array(0=>'Фиксированная', 1=>'Процент')); ?>
-            </td>
-            <td>
-                <input type="text" name="variants[<?php echo $attribute->id ?>][sku][]">
-            </td>
-            <td>
-                <a href="#" onclick="return deleteVariantRow($(this));">Удалить</a>
-            </td>
-        </tr>
-        <?php
-            endif;
-        ?>
-        <?php
-            if(isset($options)):
-            foreach($options as $o):
-        ?>
-        <tr>
-            <td>
-                <?php
-                    echo CHtml::dropDownList('variants['.$attribute->id.'][option_id][]', $o->option->id, CHtml::listData($attribute->options, 'id', 'value'));
-                ?>
-            </td>
-            <td>
-                <input type="text" name="variants[<?php echo $attribute->id ?>][price][]" value="<?php echo $o->price ?>">
-            </td>
-            <td>
-                <?php echo CHtml::dropDownList('variants['.$attribute->id.'][price_type][]', $o->price_type, array(0=>'Фиксированная', 1=>'Процент')); ?>
-            </td>
-            <td>
-                <input type="text" name="variants[<?php echo $attribute->id ?>][sku][]" value="<?php echo $o->sku ?>">
-            </td>
-            <td>
-                <a href="#" onclick="return deleteVariantRow($(this));">Удалить</a>
-            </td>
-        </tr>
-        <?php
-            endforeach;
-            endif;
-        ?>
-    </tbody>
+	<thead>
+	<tr>
+		<td colspan="6">
+			<h4>
+				<?php
+				echo CHtml::encode($attribute->title);
+				echo CHtml::link(' +', '#', array(
+					'rel'=>'#variantAttribute'.$attribute->id,
+					'onclick'=>'js: return cloneVariantRow($(this));'
+				));
+				?>
+			</h4>
+		</td>
+	</tr>
+	<tr>
+		<td>Значение</td>
+		<td>Цена</td>
+		<td>Тип цены</td>
+		<td>Артикул</td>
+		<td></td>
+	</tr>
+	</thead>
+	<tbody>
+		<?php
+			if(!isset($options)):
+		?>
+		<tr>
+			<td>
+				<?php
+					echo CHtml::dropDownList('variants['.$attribute->id.'][option_id][]', null, CHtml::listData($attribute->options, 'id', 'value'));
+				?>
+			</td>
+			<td>
+				<input type="text" name="variants[<?php echo $attribute->id ?>][price][]">
+			</td>
+			<td>
+				<?php echo CHtml::dropDownList('variants['.$attribute->id.'][price_type][]', null, array(0=>'Фиксированная', 1=>'Процент')); ?>
+			</td>
+			<td>
+				<input type="text" name="variants[<?php echo $attribute->id ?>][sku][]">
+			</td>
+			<td>
+				<a href="#" onclick="return deleteVariantRow($(this));">Удалить</a>
+			</td>
+		</tr>
+		<?php
+			endif;
+		?>
+		<?php
+			if(isset($options)):
+			foreach($options as $o):
+		?>
+		<tr>
+			<td>
+				<?php
+					echo CHtml::dropDownList('variants['.$attribute->id.'][option_id][]', $o->option->id, CHtml::listData($attribute->options, 'id', 'value'));
+				?>
+			</td>
+			<td>
+				<input type="text" name="variants[<?php echo $attribute->id ?>][price][]" value="<?php echo $o->price ?>">
+			</td>
+			<td>
+				<?php echo CHtml::dropDownList('variants['.$attribute->id.'][price_type][]', $o->price_type, array(0=>'Фиксированная', 1=>'Процент')); ?>
+			</td>
+			<td>
+				<input type="text" name="variants[<?php echo $attribute->id ?>][sku][]" value="<?php echo $o->sku ?>">
+			</td>
+			<td>
+				<a href="#" onclick="return deleteVariantRow($(this));">Удалить</a>
+			</td>
+		</tr>
+		<?php
+			endforeach;
+			endif;
+		?>
+	</tbody>
 </table>
