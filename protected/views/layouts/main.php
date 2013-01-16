04ab2dfdd964d053bdb532d6752ff15fb44edcab
firstrow <firstrow@gmail.com>
2013-01-17 01:11:13 +0200

    Fixed top menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -110,7 +110,18 @@
 	</div>
 
 	<div class="mainm">
-
+		<?php
+			Yii::import('application.modules.store.models.StoreCategory');
+			$items = StoreCategory::model()->findByPk(1)->asCMenuArray();
+			if(isset($items['items']))
+			{
+				$this->widget('application.extensions.mbmenu.MbMenu',array(
+					'cssFile'=>Yii::app()->theme->baseUrl.'/assets/css/menu.css',
+					'htmlOptions'=>array('class'=>'dropdown', 'id'=>'nav'),
+					'items'=>$items['items'])
+				);
+			}
+		?>
 	</div>
 </div>
 
