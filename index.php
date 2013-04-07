17f1137895c6a7a2ae9e0672ae36633f16bea2cc
komodo <firstrow@gmail.com>
2013-04-07 13:52:27 +0300

    Updated phing builder to support VERSION

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ else
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_TRACE_
-LEVEL') or define('YII_TRACE_LEVEL', 3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
 require $yii;
 require 'protected/components/SWebApplication.php';
