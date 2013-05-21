7d1bf909c72a3c3845d196838b136f8a03e5b45e
komodo <firstrow@gmail.com>
2013-05-22 00:14:37 +0300

    Fixed pages module

--- protected/modules/pages/config/CategoryUrlRule.php
+++ protected/modules/pages/config/CategoryUrlRule.php
@@ -8,50 +8,42 @@ Yii::import('application.modules.pages.models.PageCategory');
  */
 class CategoryUrlRule extends CBaseUrlRule
 {
-    public $connectionID = 'db';
-    public $urlSuffix    = '';
- 
-    public function createUrl($manager,$route,$params,$ampersand)
-    {
-        if($route==='pages/pages/list')
-        {
-            $url=trim($params['url'],'/');
-            unset($params['url']);
+	public $connectionID = 'db';
+	public $urlSuffix    = '';
 
-            $parts=array();
-            if(!empty($params))
-            {
-                foreach ($params as $key=>$val)
-                    $parts[]=$key.'/'.$val;
+	public function createUrl($manager,$route,$params,$ampersand)
+	{
+		if($route==='pages/pages/list')
+		{
+			$url=trim($params['url'],'/');
+			unset($params['url']);
 
-                $url .= '/'.implode('/', $parts);
-            }
+			$parts=array();
+			if(!empty($params))
+			{
+				foreach ($params as $key=>$val)
+					$parts[]=$key.'/'.$val;
 
-            return $url.$this->urlSuffix;;
-        }
-        return false;
-    }
- 
-    public function parseUrl($manager,$request,$pathInfo,$rawPathInfo)
-    {
-        if ($this->urlSuffix)
-            $pathInfo = strtr($pathInfo, array($this->urlSuffix=>''));
+				$url .= '/'.implode('/', $parts);
+			}
 
-        $parts    = explode('/', $pathInfo);
-        $pathInfo = $parts[0];
+			return $url.$this->urlSuffix;;
+		}
+		return false;
+	}
 
-        if(isset($parts[1], $parts[2]))
-        {
-            if('page'===$parts[1])
-                $_GET['page']=$parts[2];
-        }
+	public function parseUrl($manager,$request,$pathInfo,$rawPathInfo)
+	{
+		if ($this->urlSuffix)
+			$pathInfo = strtr($pathInfo, array($this->urlSuffix=>''));
 
-        if(PageCategory::countByPath($pathInfo))
-        {
-            $_GET['url'] = $pathInfo;
-            return 'pages/pages/list';
-        }
-        return false;
-    }
+		if(PageCategory::countByPath($pathInfo))
+		{
+			$_GET['url'] = $pathInfo;
+			return 'pages/pages/list';
+		}
+
+		return false;
+	}
 
 }
