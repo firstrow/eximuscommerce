3d1a11b90af71a73221c815d0a3c45d4a3715867
komodo <firstrow@gmail.com>
2013-05-28 22:22:19 +0300

    Added image test

---
+++ protected/modules/store/tests/unit/StoreImagesConfigTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class StoreImagesConfigTest extends CTestCase
+{
+	public function testSettings()
+	{
+		Yii::import('application.modules.store.components.StoreImagesConfig');
+
+		$this->assertEquals(StoreImagesConfig::get('path'), 'webroot.uploads.product');
+
+		// Test loaging from db
+		$r1=rand(800,2000);
+		$r2=rand(800,2000);
+
+		Yii::app()->settings->set(StoreImagesConfig::$settings_key, array(
+			'maximum_image_size'=>$r1.'x'.$r2
+		));
+
+		StoreImagesConfig::initialize();
+
+		$this->assertEquals(StoreImagesConfig::get('maximum_image_size'), array($r1, $r2));
+	}
+}
